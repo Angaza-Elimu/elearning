79a44010f91b310d2f3ae4922f74b3daeaeb90d1
Austin Ofor <kwadoskii@live.com>
2022-10-01 17:43:47 +0100

    added subject name to Topic selection page of revision

--- pages/revision/subjects/[subject_id].jsx
+++ pages/revision/subjects/[subject_id].jsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { validToken } from "../../../api/auth";
+import { getSubjectsApi } from "../../../api/subjects";
 import { getTopics } from "../../../api/topics";
 import { Breadcomb, Header, Layout, LearnCard } from "../../../components";
 
-export default function RevisionTopicPage({ topics }) {
+export default function RevisionTopicPage({ topics, subject }) {
   const [loading, setLoading] = useState(true);
   const { query } = useRouter();
 
@@ -16,14 +17,11 @@ export default function RevisionTopicPage({ topics }) {
   }, []);
 
   return loading ? null : (
-    <Layout title={`Pick a ${query.subject_id} Topic`}>
+    <Layout title={`Pick a topic in ${subject.subject_name}`}>
       <div>
         <Breadcomb />
 
-        <Header
-          text={`Now choose a topic in ${query.subject_id?.replace(/_/g, " ")}...`}
-          className="mt-3"
-        />
+        <Header text={`Now choose a topic in ${subject.subject_name}...`} className="mt-3" />
 
         <div className="max-w-7xl w-full mr-auto my-8">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 lg:gap-10">
@@ -58,14 +56,17 @@ export default function RevisionTopicPage({ topics }) {
   );
 }
 
-export const getServerSideProps = async ({ req: { cookies } }) => {
-  // const class_id = JSON.parse(JSON.parse(cookies["persist%3Aroot"]).profile).profile.class; //change this to class selected
+export const getServerSideProps = async ({ req: { cookies }, query }) => {
   const class_id = JSON.parse(JSON.parse(cookies["persist%3Aroot"]).grade).grade.id;
 
-  let { status, data: topics } = await getTopics(class_id, cookies.subject_id, cookies.token);
+  let { status, data: topics } = await getTopics(class_id, query.subject_id, cookies.token);
+  let { data: subject } = await getSubjectsApi(cookies.token);
+
+  subject = subject.find((s) => s.id.toString() === query.subject_id);
+
   if (status !== 200) topics = [];
 
   return {
-    props: { topics },
+    props: { topics, subject },
   };
 };
