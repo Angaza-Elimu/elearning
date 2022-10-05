202c4ece809accc1776091faf2b3881d6e81f7c1
Austin Ofor <kwadoskii@live.com>
2022-10-05 12:53:31 +0100

    input fix

--- components/Input.jsx
+++ components/Input.jsx
@@ -29,9 +29,9 @@ export default function Input({
   }, [type]);
 
   return (
-    <div className="border-2 border-neutral-800 rounded-md focus-within:border-primary-700 relative outline-none my-2 md:my-3">
+    <div className="border border-neutral-700 rounded-md focus-within:border-primary-700 relative outline-none my-1.5 md:my-3">
       <input
-        className="block w-full appearance-none focus:outline-none bg-transparent p-3 text-base placeholder-shown:text-right focus:text-left"
+        className="block w-full appearance-none focus:outline-none bg-transparent p-2 md:p-3 text-base placeholder-shown:text-right focus:text-left"
         id={label.toLowerCase().trim().split(" ").join("")}
         name={label.toLowerCase().trim().split(" ").join("")}
         onChange={onChange}
@@ -49,7 +49,7 @@ export default function Input({
       />
       <label
         htmlFor={label.toLowerCase().trim().split(" ").join("")}
-        className="absolute top-2.5 left-3 duration-300 origin-0 bg-[#FBFBFB] md:text-lg text-neutral-500 font-medium px-1 select-none"
+        className="absolute scale-90 top-2.5 left-3 duration-300 origin-0 bg-[#FBFBFB] md:text-lg text-neutral-500 font-medium md:px-1 select-none"
       >
         {label}
       </label>
